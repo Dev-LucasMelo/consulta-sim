efd8e9f9dc1979d52de33e4de5f3e53a946cb287
Lucas Melo <lmelo072002@gmail.com>
2023-10-30 21:16:03 -0300

    validates de endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,11 @@
 class Endereco < ApplicationRecord
+  #relacionamentos
   belongs_to :paciente
+
+  #validates
+  validates :cep, presence: true, length: { maximum: 8, minimum:8, message: "Deve conter exatamente 8 digitos" },numericality: { only_integer: true }
+  validates :cidade, presence: true, format: { without: /\d/, message: "Nome não pode conter números" },length: { minimum: 4 }  
+  validates :bairro, presence: true, format: { without: /\d/, message: "Nome não pode conter números" },length: { minimum: 5 } 
+  validates :logradouro, presence: true, length: { minimum: 5 }
+  validates :complemento, presence: true,length: { minimum: 10, maximum: 40}
 end
