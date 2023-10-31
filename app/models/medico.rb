dd320ffae7174e8f6a87ba2ea10fe8b0aded8874
Lucas Melo <lmelo072002@gmail.com>
2023-10-30 23:39:20 -0300

    validates de medico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,3 +1,12 @@
 class Medico < ApplicationRecord
+    #relacionamentos
     has_many :consulta, dependent: :destroy
+
+    #validates
+    validates :nome , presence: true, length: { minimum: 10 , message: "nao pode ter menos que 10 digitos"} , format: { without: /\d/, message: "Nome não pode conter números" }
+    validates :cpf , presence: true, length: { minimum: 11, maximum:11 ,message: "deve ter exatamente 11 digitos" }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "não é um formato de e-mail válido" }, confirmation: { case_sensitive: false }
+    validates :especialidade, presence:true,  length: { minimum: 5, maximum: 20 }, format: { without: /\d/, message: "não pode conter números" }
+    validates :crm, presence: true, length: { minimum: 8, maximum:8 ,message: "deve ter exatamente 8 digitos" }
+
 end
