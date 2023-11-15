425cc5bd7485e3a83e004bff582ed602f41336f4
Lucas Melo <lmelo072002@gmail.com>
2023-11-15 14:17:57 -0300

    implementando testes de especialidade e crm referentes ao model de medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "medico com crm invalido" do
+    medico = Medico.new nome: "Dr Messias", cpf: "71493574400", 
+    email: "drmessias@gmail.com", especialidade: "pediatra",
+    crm: "1234567"
+    assert_not medico.save
+  end
+  test "medico com especialidade invalida" do
+    medico = Medico.new nome: "Dr Messias", cpf: "71493574400", 
+    email: "drmessias@gmail.com", especialidade: "abc",
+    crm: "12345678"
+    assert_not medico.save
+  end
 end
