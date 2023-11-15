eb578ba22a5f31eed06d2bccb4864a9f3dd0c743
Lucas Melo <lmelo072002@gmail.com>
2023-11-15 14:01:51 -0300

    implementando testes de cpf e nome para o model de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-   test 'testando o basico' do
-     paciente = Paciente.new  
-     assert_not paciente.save
+   test 'paciente com nome invalido' do
+     paciente = Paciente.new  nome: "t", email: "lucasmelo@gmail.com" , data_nascimento: "2002-11-07", cpf: "71493574400" 
+     assert_not paciente.save, "paciente nao pode ser salvo"
    end
+
+   test 'paciente com cpf invalido' do
+    paciente = Paciente.new  nome: "lucas melo", email: "lucasmelo@gmail.com" , data_nascimento: "2002-11-07", cpf: "7149357440" 
+    assert_not paciente.save, "paciente nao pode ser salvo"
+  end
 end
