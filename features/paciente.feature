7106464e215a7f8fbcd7c5a367b3a84c5c5bf46d
Lucas Melo <lmelo072002@gmail.com>
2023-11-19 02:11:46 -0300

    correcao de um passo do scenario de pacientes

--- features/paciente.feature
+++ features/paciente.feature
@@ -5,12 +5,12 @@ Feature:
 
   Scenario: cadastrar paciente
     Given Estou na pagina de cadastrar paciente
-    When eu preencho os dados nome 'Lucas Messias' email 'Lucasteste@gmail.com' cpf '71493574400' data_nascimento '2002-11-07' cep '55294310' cidade 'Garanhuns' bairro 'Brasilia' logradouro 'rua pedro rodrigues' complemento 'teste complemento'
+    When eu preencho os dados nome 'doutor lucas' email 'Lucas@gmail.com' data_nascimento '2002-11-07' cpf '71493574400' cep '55294310' cidade 'garanhuns' bairro 'brasilia' logradouro 'testeee' complemento 'teste complemento'
     And eu clico no botao enviar
-    Then vejo que cadastro do paciente foi efetuado com sucesso
+    Then vejo que os dados do paciente estao corretos
 
 Scenario: atualizar paciente
-    Given Estou na pagina de cadastrar paciente
-    When eu preencho os dados nome 'Lucas Melo' email 'Lucasteste@gmail.com' cpf '71493574400' data_nascimento '2002-11-07' cep '55294310' cidade 'Garanhuns' bairro 'Brasilia' logradouro 'rua pedro rodrigues' complemento 'teste complemento'
+    Given Estou na pagina de atualizar o paciente
+    When eu preencho os dados nome 'doutor lucas' email 'Lucas@gmail.com' data_nascimento '2002-11-07' cpf '71493574400' cep '55294310' cidade 'garanhuns' bairro 'brasilia' logradouro 'testeee' complemento 'teste complemento'
     And eu clico no botao enviar
-    Then vejo que os dados do paciente foram atualizados com sucesso
+    Then vejo que os dados do paciente estao corretos
--- features/step_definitions/paciente_steps.rb
+++ features/step_definitions/paciente_steps.rb
@@ -1,7 +1,15 @@
-Given('Estou na pagina de cadastrar paciente') do
+  Given('Estou na pagina de cadastrar paciente') do
     visit '/pacientes/new'
     expect(page).to have_current_path('/pacientes/new')
   end
+
+  Given('Estou na pagina de atualizar o paciente') do
+    paciente = Paciente.create(nome: 'lucas melo', email: 'teste@gmail.com', data_nascimento: '2002-11-07', cpf: '71493574400', cep: '55294310', cidade: 'garanhuns', bairro: 'brasilia', logradouro: 'testeee', complemento: 'teste complemento')
+
+    visit edit_paciente_path(paciente)
+    expect(page).to have_current_path(edit_paciente_path(paciente))
+  end
+
   When('eu preencho os dados nome {string} email {string} data_nascimento {string} cpf {string} cep {string} cidade {string} bairro {string} logradouro {string} complemento {string}') do |nome, email, cpf, data_nascimento, cep, cidade, bairro, logradouro, complemento|
     fill_in 'paciente[nome]', :with => nome
     fill_in 'paciente[email]', :with => email
@@ -14,16 +22,14 @@ Given('Estou na pagina de cadastrar paciente') do
     fill_in 'paciente[endereco_attributes][complemento]', :with => complemento
   end
   
-  When('eu clico no botao criar paciente') do
+  When('eu clico no botao enviar') do
     click_button 'Enviar'
   end
   
-  Then('vejo que cadastro do paciente foi efetuado com sucesso') do
-    expect(page).to have_current_path(pacientes_path + '/' + Paciente.last.id.to_s)
+  Then('vejo que os dados do paciente estao corretos') do
+    expect(page).to have_current_path(pacientes_path + '.' + Paciente.last.id.to_s)
   end
 
-  Then('vejo que os dados do paciente foram atualizados com sucesso') do
-    expect(page).to have_current_path(pacientes_path + '/' + Paciente.last.id.to_s)
-  end
+ 
   
  
